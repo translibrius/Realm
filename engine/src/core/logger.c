24c298ef544b2e7548145cc4918a4eef5be459de
Daumantas <daumantas.balakauskas@gmail.com>
2025-11-21 16:40:08 +0200

    linux platform stuff

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -2,46 +2,45 @@
 
 #include "platform/platform.h"
 
-#include "util/str.h"
 #include "util/assert.h"
+#include "util/str.h"
 
 typedef struct logger_state {
-    rl_arena log_arena; // Reset per log
+  rl_arena log_arena; // Reset per log
 } logger_state;
 
 static logger_state state;
 
 b8 logger_system_start() {
-    rl_arena_create(MiB(2), &state.log_arena);
-    RL_INFO("Logger system started!");
+  rl_arena_create(MiB(2), &state.log_arena);
+  RL_INFO("Logger system started!");
 
-    return true;
+  return true;
 }
 
 void logger_system_shutdown() {
-    rl_arena_destroy(&state.log_arena);
-    RL_INFO("Logger system has been shutdown...");
+  rl_arena_destroy(&state.log_arena);
+  RL_INFO("Logger system has been shutdown...");
 }
 
-void log_output(const char* message, LOG_LEVEL level, ...) {
-    rl_arena_reset(&state.log_arena);
+void log_output(const char *message, LOG_LEVEL level, ...) {
+  rl_arena_reset(&state.log_arena);
 
-    const char* level_strs[] = {
-        "[INFO]: ", "[DEBU]: ", "[TRAC]: ",
-        "[WARN]: ", "[ERRO]: ", "[FATA]: "
-    };
+  const char *level_strs[] = {
+      "[INFO]: ", "[DEBU]: ", "[TRAC]: ", "[WARN]: ", "[ERRO]: ", "[FATA]: "};
 
-    va_list args;
-    va_start(args, level);
-    char* formatted = cstr_format_va(&state.log_arena, message, args);
-    va_end(args);
+  va_list args;
+  va_start(args, level);
+  char *formatted = cstr_format_va(&state.log_arena, message, args);
+  va_end(args);
 
-    // Add level and newline
-    char* final_message = cstr_format(&state.log_arena, "%s%s\n", level_strs[level], formatted);
-    
-    platform_console_write(final_message, level);
+  // Add level and newline
+  char *final_message =
+      cstr_format(&state.log_arena, "%s%s\n", level_strs[level], formatted);
 
-    if (level == LOG_FATAL) {
-        debugBreak();
-    }
+  platform_console_write(final_message, level);
+
+  if (level == LOG_FATAL) {
+    debugBreak();
+  }
 }
