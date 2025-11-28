765796a4098eed40b95dfa32080377c1606820a9
Daumantas <daumantas.balakauskas@gmail.com>
2025-11-28 16:47:12 +0200

    Utility for random numbers

---
+++ engine/src/util/rand.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdlib.h>
+#include "platform/platform.h"
+
+#include "defines.h"
+
+inline i64 rand_int_range(i64 from, i64 to) {
+    static bool seeded = false;
+    if (!seeded) {
+        srand(platform_get_absolute_time());
+        seeded = true;
+    }
+
+    return rand() % (to - from + 1) + from;
+}
+
+inline float rand_float01() {
+    return rand() / (float)RAND_MAX;
+}
