799815f8f879e814a4e1622ae73bbdd0adc3be17
friezy <daumantas.balakauskas@gmail.com>
2025-12-02 22:29:50 +0200

    Check if file/dir exists

--- engine/src/platform/io/file_io_win32.c
+++ engine/src/platform/io/file_io_win32.c
@@ -1,6 +1,28 @@
 #include "platform/io/file_io.h"
+#include "util/str.h"
+#include "vendor/glad/glad_wgl.h"
 
-b8 file_exists(const char *path) {
-    (void)path;
-    return true;
+#ifdef PLATFORM_WINDOWS
+
+typedef struct file_system_state {
+    rl_arena file_arena;
+} file_system_state;
+
+b8 file_dir_exists(const char *path) {
+    rl_arena scratch;
+    b8 found = false;
+
+    rl_arena_create(MiB(1), &scratch);
+    rl_string_create(&scratch, path);
+
+    WIN32_FIND_DATA found_data;
+    HANDLE h = FindFirstFileA(path, &found_data);
+
+    if (h != INVALID_HANDLE_VALUE)
+        found = true;
+
+    rl_arena_destroy(&scratch);
+    return found;
 }
+
+#endif
