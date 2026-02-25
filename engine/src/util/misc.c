bfa557ff54c5049bb709a92163390415d053add2
daumantasb <daumantasb@wix.com>
2026-02-25 14:36:45 +0200

    Fix rand.h ODR violation, preserve RL_WARN in release, fix hardcoded OS name

--- engine/src/util/misc.c
+++ engine/src/util/misc.c
@@ -4,7 +4,7 @@ void log_system_info() {
     platform_info *info = platform_get_info();
     RL_DEBUG("System details: ");
     RL_DEBUG("----------------------------");
-    RL_DEBUG("Operating system: Windows | Build: %d | Version: %d.%d", info->build_number, info->version_major, info->version_minor);
+    RL_DEBUG("Operating system: %s | Build: %d | Version: %d.%d", info->platform_name, info->build_number, info->version_major, info->version_minor);
     RL_DEBUG("Arch: %s", info->arch);
     RL_DEBUG("Page size: %d", info->page_size);
     RL_DEBUG("Logical processors: %d", info->logical_processors);
